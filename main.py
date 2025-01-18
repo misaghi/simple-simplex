a97b68317143fe799e78174a849e409c55a35167
university-misaghi <university.a.h.misaghi@gmail.com>
2025-01-18 16:01:48 +0330

    Added a mechanism to write output to a file.

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from simplex.createSlackForm import createSlackForm
 from simplex.simplex import simplex
 from simplex.phaseOne import phaseOne
 import pathlib
+import sys
 
 
 def main():
     path = pathlib.Path(__file__).parent / 'inputs'
     dir = path.walk().__next__()
+    original_stdout = sys.stdout
+    result_file = open(path / '..' / 'result.txt', 'w')
+    sys.stdout = result_file
     for file in dir[2]:
         print(f'_______________________Solving {file}_______________________')
         input_path = path / file
@@ -25,6 +29,9 @@ def main():
                 normalized_objective, normalized_constraints)
             if phase_one_constraints:
                 simplex(normalized_objective, phase_one_constraints)
+
+    sys.stdout = original_stdout
+    result_file.close()
 
 
 if __name__ == '__main__':
